1a56652b3cba6bd1e012663e580cb4287ed971e3
Arslaaan <aochirov@gmail.com>
2023-04-29 13:29:00 +0300

    server template refactor

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,1 +1,19 @@
 #include "Server.h"
+
+Status Server::set(ServerContext* context, const Key* request, Value* reply)  {
+        return Status::OK;
+}
+
+Status Server::get(ServerContext* context, const Key* request, Value* reply) {
+        return Status::OK;
+}
+
+Status Server::remove(ServerContext* context, const Key* request,
+                      Value* reply) {
+        return Status::OK;
+}
+
+Status Server::size(ServerContext* context, const Empty* request,
+                    DictionarySizeInfo* reply) {
+        return Status::OK;
+}
--- server/Server.h
+++ server/Server.h
@@ -1,5 +1,24 @@
 #pragma once
 
-class Server {
+#include <proto/dictionary.grpc.pb.h>
 
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using namespace dictionary;
+
+class Server final : public Storage::Service {
+    Status set(ServerContext* context, const Key* request,
+               Value* reply) override;
+
+    Status get(ServerContext* context, const Key* request,
+               Value* reply) override;
+
+    Status remove(ServerContext* context, const Key* request,
+                  Value* reply) override;
+
+    Status size(ServerContext* context, const Empty* request,
+                DictionarySizeInfo* reply) override;
 };
